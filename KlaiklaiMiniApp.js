7ca3bcd9b51960fa70b6810f02554862baa8a7ff
phattanothai <zeref38@gmail,com>
2024-01-05 10:35:45 +0700

    init

---
+++ KlaiklaiMiniApp.js
@@ -0,0 +1,113 @@
+// code = 1 ===> init app
+// code = 2 ===> earn reward
+
+class KlaiklaiMiniApp {
+  isDevMode =
+    location.hostname === "localhost"
+      ? true
+      : location.hostname === "127.0.0.1"
+      ? true
+      : !window?.ReactNativeWebView?.postMessage
+      ? true
+      : false;
+  appToken = "";
+  payload = {
+    code: 0,
+    data: {},
+  };
+  rewardList = [];
+  appName;
+
+  constructor(appName) {
+    this.appName = appName;
+    if (!this.isDevMode) {
+      this.payload = {
+        code: 4,
+        payload: {
+          appName: this.appName,
+        },
+      };
+      window.ReactNativeWebView.postMessage(JSON.stringify(this.payload));
+    }
+  }
+
+  initApp(appId) {
+    this.appId = appId;
+    if (this.isDevMode) {
+      this.appToken = "testMode";
+      this.rewardList = [
+        {
+          id: 1,
+          name: "Coin",
+          value: 10,
+        },
+        {
+          id: 2,
+          name: "",
+        },
+        {
+          id: 3,
+          name: "Coin",
+          value: 10,
+        },
+        {
+          id: 4,
+          name: "",
+        },
+        {
+          id: 5,
+          name: "Coin",
+          value: 10,
+        },
+        {
+          id: 6,
+          name: "",
+        },
+      ];
+    } else {
+      this.payload = {
+        code: 1,
+        payload: {
+          appId: this.appId,
+        },
+      };
+      window.ReactNativeWebView.postMessage(JSON.stringify(this.payload));
+    }
+  }
+
+  setAppToken(token) {
+    this.appToken = token;
+  }
+
+  earnReward(reward) {
+    alert("ยินดีด้วย คุณได้รับรางวัล " + reward?.value + " " + reward?.name);
+    if (!this.isDevMode) {
+      setTimeout(() => {
+        this.payload = {
+          code: 2,
+          payload: {
+            rewardId: reward?.id,
+          },
+        };
+        window.ReactNativeWebView.postMessage(JSON.stringify(this.payload));
+      }, 2000);
+    }
+  }
+
+  closeApp() {
+    alert("เสียใจด้วย คุณไม่ได้รับรางวัล");
+    if (!this.isDevMode) {
+      setTimeout(() => {
+        this.payload = {
+          code: 3,
+          payload: {},
+        };
+        window.ReactNativeWebView.postMessage(JSON.stringify(this.payload));
+      }, 2000);
+    }
+  }
+
+  setRewardList(rewardList) {
+    this.rewardList = rewardList;
+  }
+}
